53e1fe5916b70badf617b0906616fce6ca674435
Danilo Guanabara <daniloguanabara@nyanheroes.com>
2022-07-21 19:58:12 -0300

    UE Game & Integration

---
+++ Game/Plugins/Wallet/Source/Wallet/Public/Wallet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Wallet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WALLET_API UWallet : public UObject
+{
+	GENERATED_BODY()
+	
+};
